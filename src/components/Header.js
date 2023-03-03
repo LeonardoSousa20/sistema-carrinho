81995f12175cf488489624bd25a2f0d120089bbf
Leonardo Sousa <leonardo.sousa7120@gmail.com>
2023-03-03 12:44:30 -0300

    commit na branch dev

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="w-full h-16 bg-black text-white flex justify-center items-center px-5 shadow-lg fixed top-0">
-      <p className=" font-bold  text-lg font-montserrat">
-        DESAFIO SHOPPING CART
-      </p>
+      <p className=" font-bold  text-lg font-montserrat">SHOPPING CART</p>
     </div>
   );
 };
